6935a0e840a329d3837e24af04e494642271e3f7
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2023-04-17 23:54:13 +0300

    add comments on score class

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,34 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    This class is used in order to calculate 
+     and show the last score of the user
+*/
 public class Score
 {
-    public int currentScore;
-    public int currentScorelast;
+    public int currentScore; // Shows current score of the user
+    public int currentScorelast; // Shows the user's score on game finish
     public Score()
     {
 
     }
 
+    /*
+        Calulates to current score of the user acording to the relevant formula
+        by using current army (ball) count of the user
+    */
     public void SetScore()
     {
-        int count = Gamemanager.GameManagerInstance.Balls.Count;
+        int count = Gamemanager.GameManagerInstance.Army.Count;
         int level = Gamemanager.GameManagerInstance.dataBase.currentLevel;
         currentScore = (int)(1+ (level/10.0)) * (count+1);
-        // switch (type)
-        // {
-        //     case "Gates":
-        //     currentScore += 5;
-        //     break;
-
-
-        //     default:
-        //     break;
-        // }
     }
     public void ShowScore()
     {
-        int count = Gamemanager.GameManagerInstance.Balls.Count;
+        int count = Gamemanager.GameManagerInstance.Army.Count;
         int level = Gamemanager.GameManagerInstance.dataBase.currentLevel;
         currentScorelast = (int)(1+ (level/10.0)) * (count+1);
     }
