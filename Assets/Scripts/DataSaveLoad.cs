3e05816ae5fcc3e1f12c18c873da80a077eeb64e
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2023-04-17 22:42:10 +0300

    add comments to datasaveload

--- Assets/Scripts/DataSaveLoad.cs
+++ Assets/Scripts/DataSaveLoad.cs
@@ -2,23 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+
+/* 
+    This class is used for saving and 
+     loading the preferences, levels, scores of the player
+*/
 public class DataSaveLoad
 {
+    // Scores of user
     public int totalScore;
     public int HighestScore;
     public int currentLevel;
 
+    // Resulotion preferences
     public int resulotionWidth;
     
     public int resulotionHeight;
 
-    public string mute;
+    public string mute; // Sound mute preference
+
+    /*
+        Constructor method works on a DataSaveLoad class created that allows
+         data read and write operations. Also reads stored data on creation
+    */
     public DataSaveLoad()
     {
         InitiliazeKeys();
         ReadFile();
     }
 
+    /*
+        Playerprefs is a built-in class in the Unity that allows storing user
+        information on the user's device. Stored data can be acessed by using
+        saved the key value which is used for saving relevant data.
+    */
     private void ReadFile()
     {
 
@@ -29,58 +46,26 @@ public class DataSaveLoad
         this.resulotionWidth = PlayerPrefs.GetInt("resulotionWidth");
         this.resulotionHeight = PlayerPrefs.GetInt("resulotionHeight");
         this.mute =  PlayerPrefs.GetString("mute");
-            // try
-            // {
-            //     StreamReader sr = new StreamReader(Application.dataPath+"/database.txt");//Application.dataPath
-            //     //StreamReader sr = new StreamReader("Assets/Scripts/Saves/database.txt");
-            //    // Debug.LogWarning(Application.dataPath);
-
-            //     // this.totalScore = long.Parse(sr.ReadLine());
-            //     // this.HighestScore = long.Parse(sr.ReadLine());
-            //     // this.currentLevel = int.Parse(sr.ReadLine());
-
-
-            //     sr.Close();
-            // }
-            // catch(System.Exception e)
-            // {
-            //     Debug.LogWarning("Exception: " + e.Message);
-            // }
-            // finally
-            // {
-            //     Debug.LogWarning("Executing finally block.");
-            // }
     }
     
-
+    /*
+        Similiar to read however this time Playerprefs is used in a key-pair
+        relation in order to store relevant data of the user
+    */
     public void WriteFile()
     {
         PlayerPrefs.SetInt("total_score",this.totalScore);
         PlayerPrefs.SetInt("highest_score",this.HighestScore);
         PlayerPrefs.SetInt("current_level",this.currentLevel);
         PlayerPrefs.SetString("mute",this.mute);
-        // try
-        // {
-        //     StreamWriter sw = new StreamWriter(Application.dataPath+"/database.txt");//"/Scripts/Saves/database.txt"
-
-        //     // sw.WriteLine(this.totalScore);
-        //     // sw.WriteLine(this.HighestScore);
-        //     // sw.WriteLine(this.currentLevel);
-
-
-        //     sw.Close();
-        // }
-        // catch(System.Exception e)
-        // {
-        //     Debug.LogWarning("Exception: " + e.Message);
-        // }
-        // finally
-        // {
-        //     Debug.LogWarning("Executing finally block.");
-        // }
     }
+    /*
+       If user is playing the game first time this method 
+        initializes user's data by assigning default values
+    */
     private void InitiliazeKeys()
     {
+        // Default values are set if the relevant data is not exists in the user's device
         if(!PlayerPrefs.HasKey("total_score"))
         {
             PlayerPrefs.SetInt("total_score",5);
@@ -108,79 +93,3 @@ public class DataSaveLoad
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using System.IO;
-// public class DataSaveLoad
-// {
-//     public long totalScore;
-//     public long HighestScore;
-//     public int currentLevel;
-
-//     public DataSaveLoad()
-//     {
-//         ReadFile();
-//     }
-
-//     private void ReadFile()
-//     {
-//             try
-//             {
-//                 StreamReader sr = new StreamReader(Application.dataPath+"/database.txt");
-//                 //StreamReader sr = new StreamReader("Assets/Scripts/Saves/database.txt");
-//                 Debug.LogWarning(Application.dataPath);
-//                 this.totalScore = long.Parse(sr.ReadLine());
-//                 this.HighestScore = long.Parse(sr.ReadLine());
-//                 this.currentLevel = int.Parse(sr.ReadLine());
-//                 sr.Close();
-//             }
-//             catch(System.Exception e)
-//             {
-//                 Debug.LogWarning("Exception: " + e.Message);
-//             }
-//             finally
-//             {
-//                 Debug.LogWarning("Executing finally block.");
-//             }
-//     }
-    
-
-//     public void WriteFile()
-//     {
-//         try
-//         {
-//             StreamWriter sw = new StreamWriter(Application.dataPath+"/database.txt");//"/Scripts/Saves/database.txt"
-//             sw.WriteLine(this.totalScore);
-//             sw.WriteLine(this.HighestScore);
-//             sw.WriteLine(this.currentLevel);
-//             sw.Close();
-//         }
-//         catch(System.Exception e)
-//         {
-//             Debug.LogWarning("Exception: " + e.Message);
-//         }
-//         finally
-//         {
-//             Debug.LogWarning("Executing finally block.");
-//         }
-//     }
-// }
-
